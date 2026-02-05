b0843b77adaec009923da905461a025518549736
Velasco_17 <rvelascod17@gmail.com>
2026-02-05 16:42:31 -0500

    setup project and updated readme
    setup project and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FlxIsaac-Repentance
 A port of  The Binding of Isaac: Repentance on HaxeFlixel!, for now the port includes the Baby Plum boss fight
+
+## Owners
+
+- Coco.Code! [Programming]
+- AssmanBruh! [Art, Code, Designer]
--- source/Main.hx
+++ source/Main.hx
@@ -1,13 +1,46 @@
 package;
 
 import flixel.FlxGame;
+import flixel.FlxState;
 import openfl.display.Sprite;
+
+typedef GameConfigs = {
+	var width:Int;
+	var height:Int;
+	var initialState:Class<FlxState>;
+	var FPS:Int;
+	var skipSplash:Bool;
+	var startFullscreen:Bool;
+}
 
 class Main extends Sprite
 {
+	var gameConfig:GameConfigs;
+
+	function gameSetup(){
+        gameConfig = {
+			width: 0,
+			height: 0,
+			initialState: PlayState,
+			FPS: 60,
+			skipSplash: true,
+			startFullscreen: false
+		}
+	}
+
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+		gameSetup();
+		var daGame = new FlxGame(
+			gameConfig.width,
+			gameConfig.height, 
+			gameConfig.initialState,
+			gameConfig.FPS, 
+			gameConfig.FPS, 
+			gameConfig.skipSplash, 
+			gameConfig.startFullscreen
+		);
+		addChild(daGame);
 	}
 }
